675cddbbec85bc29db96706769ac05d01f0ecd42
Avila <guilherme.medeiros.avila@gmail.com>
2023-03-31 18:19:16 -0300

    commit_31_03_23_v2

---
+++ sort_algorithm/bubble_sort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+# Bubble Sort
+- Ordenação para pequenos conjuntos de dados
+
+*/ 
+
+
+int main() {
+
+    cout << "Bubble Sort: \n";
+    const int n = 5;
+    int vect[n] = {5,3,1,7,2};
+    // compara de dois em dois, e faz inversão caso um seja menor
+
+    for(int i=0;i<n;i++){
+ 
+        for(int j=0;j<n-1;j++){
+            if(vect[j] > vect[j+1]){
+                int aux = vect[j];
+                vect[j] = vect[j+1];
+                vect[j+1] = aux;
+            }
+        }
+ 
+    }
+
+    cout << "| ";
+    for(int i=0;i<n;i++){
+        cout << vect[i] << " | " ;
+    }
+
+    return 0;
+    
+}
---
+++ sort_algorithm/insertion_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+    /* 
+    # InsertionSort
+
+    ## DESCRICAO
+        Ordenação de cartas de baralho com as mãos: pega uma carta e a coloca em seu lugar, sempre deixando em ordem;
+    
+    ## PERFORMANCE
+        Pior caso: O(N^2) 
+        Eficiente para conjuntos pequenos de dados
+        Estável: não altera a ordem de dados iguais
+        Capaz de ordenar os dados em demanda (tempo real)
+    */
+int main() {
+
+    const int n = 4;
+    int vect[n] = {1,14,31,2};
+
+    for(int i=1;i<n;i++){
+        int aux = vect[i];
+    }
+
+    return 0;
+    
+}
